422509defa22c3b35c12fa71d79007936eeb38d4
Marco Bonelli <marco@mebeim.net>
2018-12-05 06:39:16 +0100

    2018: add day 5 input and original solution.

---
+++ 2018/original/05.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import utils
+from collections import defaultdict
+from string import ascii_lowercase
+
+utils.setup(2018, 5)
+
+fin = utils.get_input()
+ans = 0
+ans2 = 0
+
+##################################################
+
+units = list(map(ord, fin.read().rstrip()))
+
+def react():
+	global units
+
+	change = False
+
+	for i in range(len(units)-1):
+		if units[i] == units[i+1] + 32 or units[i] + 32 == units[i+1]:
+			units[i] = 0
+			units[i+1] = 0
+			change = True
+
+	units = list(filter(bool, units))
+
+	return change
+
+while react():
+	continue
+
+ans = 0
+for c in units:
+	if c:
+		ans += 1
+
+utils.submit_answer(1, ans)
+
+results = []
+
+fin = utils.get_input()
+iunits = list(map(ord, fin.read().rstrip()))
+
+for l in map(ord, ascii_lowercase):
+	units = iunits[:]
+	print('--',chr(l),'->', end=' ')
+
+	for i in range(len(units)):
+		if (units[i] == l - 32 or units[i] == l):
+			units[i] = 0
+
+	units = list(filter(bool, units))
+
+	while react():
+		continue
+
+	print(len(units))
+
+	results.append(len(units))
+
+ans2 = min(results)
+
+utils.submit_answer(2, ans2)
