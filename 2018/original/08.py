e3e93583ac885d855008dc7bd1c93ce5c3da8d78
Marco Bonelli <marco@mebeim.net>
2018-12-08 08:15:27 +0100

    2018: add day 8 input and original solution.

---
+++ 2018/original/08.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import utils
+
+utils.setup(2018, 8, dry_run=True)
+fin = utils.get_input()
+
+##################################################
+
+def scan(n, cur_id):
+	nchilds = n[0]
+	nmeta = n[1]
+
+	if nchilds == 0:
+		tree[cur_id] = ([], n[2:2+nmeta])
+		return sum(tree[cur_id][1]), 2 + nmeta
+
+	cur_len = 2
+	meta_sum = 0
+	childs = []
+
+	for _ in range(nchilds):
+		child_id = cur_id + cur_len
+		childs.append(child_id)
+
+		child_meta_sum, child_len = scan(n[cur_len:], child_id)
+		meta_sum += child_meta_sum
+		cur_len += child_len
+
+	tree[cur_id] = (childs, n[cur_len:cur_len+nmeta])
+
+	return meta_sum + sum(tree[cur_id][1]), cur_len + nmeta
+
+def tree_value(root):
+	me = tree[root]
+	nchilds = len(me[0])
+
+	if nchilds:
+		tot = 0
+		for child_index in me[1]:
+			if 1 <= child_index <= nchilds:
+				tot += tree_value(me[0][child_index - 1])
+
+		return tot
+
+	return sum(me[1])
+
+tree = {}
+nums = list(map(int, fin.read().split()))
+
+ans, _ = scan(nums, 0)
+utils.submit_answer(1, ans)
+
+# for k, v in tree.items():
+# 	print(k, ':', v)
+
+value = tree_value(0)
+utils.submit_answer(2, value)
