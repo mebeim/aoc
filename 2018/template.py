2b537bbee285f05a17193b85bceb40b8ff7949a1
Marco Bonelli <marco@mebeim.net>
2018-12-09 09:20:21 +0100

    Add more useful stuff to template script.

--- 2018/template.py
+++ 2018/template.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
 import utils
+import heapq
+import blist
 
-utils.setup(2018, xxx)
+from string import *
+from collections import defaultdict, deque, namedtuple, Counter
+
+utils.setup(2018, xxx, dry_run=True)
 fin = utils.get_input()
 
 ##################################################
+
+
+# nums = list(map(int, fin))
+# strings = list(map(str.strip, fin))
+# matrix = list(map(lambda l: list(map(int, l.split())), fin))
 
 
 
