94d601c49cff61abf8467ce4789b1d2ace2910b0
Marco Bonelli <marco@mebeim.net>
2018-12-09 10:03:09 +0100

    2018: add day 9 clean solution.

---
+++ 2018/09.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import utils
+from collections import deque
+
+def play(nplayers, max_marble):
+	scores = [0] * nplayers
+	marbles = deque([0])
+
+	for cur_marble in range(1, max_marble + 1):
+		if cur_marble % 23:
+			marbles.rotate(-1)
+			marbles.append(cur_marble)
+		else:
+			marbles.rotate(7)
+			scores[cur_marble % nplayers] += cur_marble + marbles.pop()
+			marbles.rotate(-1)
+
+	return max(scores)
+
+
+utils.setup(2018, 9, dry_run=True)
+fin = utils.get_input()
+
+s = fin.read().split()
+nplayers, last_marble = int(s[0]), int(s[6])
+
+winner1 = play(nplayers, last_marble)
+# assert winner1 == 398730
+
+utils.submit_answer(1, winner1)
+
+winner2 = play(nplayers, last_marble * 100)
+# assert winner2 == 3349635509
+
+utils.submit_answer(2, winner2)
