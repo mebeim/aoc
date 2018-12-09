86b892bd24c3100021530c88a475f6c49be0daeb
Marco Bonelli <marco@mebeim.net>
2018-12-09 09:19:53 +0100

    2018: add day 9 input and original solution.

---
+++ 2018/original/09.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import utils
+import sys
+
+from blist import blist
+
+utils.setup(2018, 9, dry_run=True)
+fin = utils.get_input()
+
+##################################################
+
+s = fin.read().split()
+nplayers, last_marble = int(s[0]), int(s[6])
+
+marbles = blist([0])
+cur_marble = 0
+cur_idx = 0
+cur_player = 1
+scores = {}
+
+# nplayers = 10
+# last_marble = 1618
+
+for i in range(1, nplayers+1):
+	scores[i] = 0
+
+while cur_marble < last_marble:
+
+	cur_marble += 1
+
+	# sys.stdout.write(str(cur_marble) + '\r')
+
+	if cur_marble % 23 != 0:
+		cur_idx = (cur_idx + 1) % len(marbles) + 1
+		marbles.insert(cur_idx, cur_marble)
+	else:
+		cur_idx = (cur_idx - 7) % len(marbles)
+		points = cur_marble + marbles.pop(cur_idx)
+
+		scores[cur_marble % nplayers + 1] += points
+
+		# print('\n>>>', cur_marble, points, last_marble)
+
+
+winner_score = max(scores.values())
+utils.submit_answer(1, winner_score)
+
+
+marbles = blist([0])
+cur_marble = 0
+cur_idx = 0
+cur_player = 1
+scores = {}
+
+last_marble *= 100
+
+for i in range(1, nplayers+1):
+	scores[i] = 0
+
+while cur_marble < last_marble:
+
+	cur_marble += 1
+
+	# sys.stdout.write(str(cur_marble) + '\r')
+
+	if cur_marble % 23 != 0:
+		cur_idx = (cur_idx + 1) % len(marbles) + 1
+		marbles.insert(cur_idx, cur_marble)
+	else:
+		cur_idx = (cur_idx - 7) % len(marbles)
+		points = cur_marble + marbles.pop(cur_idx)
+
+		scores[cur_marble % nplayers + 1] += points
+
+
+winner_score = max(scores.values())
+utils.submit_answer(2, winner_score)
