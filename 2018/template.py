1b257c64dbc9d85617a5db9a5ec4d1c34b64b71e
Marco Bonelli <marco@mebeim.net>
2018-12-13 00:17:01 +0100

    Update template to display input file by default.

--- 2018/template.py
+++ 2018/template.py
@@ -9,9 +9,9 @@ from collections import defaultdict, deque, namedtuple, Counter
 
 utils.setup(2018, xxx, dry_run=True)
 fin = utils.get_input()
+print(*fin)
 
 ##################################################
-
 
 # nums = list(map(int, fin))
 # strings = list(map(str.strip, fin))
