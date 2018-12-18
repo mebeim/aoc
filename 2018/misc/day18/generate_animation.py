8a1b87a1b6e6ae36542be7b7d1cbe2c26b71eaab
Marco Bonelli <marco@mebeim.net>
2018-12-19 00:44:32 +0100

    2018 d18: coolest animation ever!

--- 2018/misc/day18/generate_animation.py
+++ 2018/misc/day18/generate_animation.py
@@ -4,11 +4,11 @@ import os
 import sys
 import copy
 import pickle
-from PIL import Image, ImageDraw
 from subprocess import call
 from multiprocessing import Pool
 from collections import namedtuple
 from random import choice as randchoice
+from PIL import Image, ImageDraw
 
 def iter_diagonals(sz, matrix=None):
 	for diag_len in range(sz+1):
@@ -37,15 +37,21 @@ def gen_base_frame():
 
 	for i in range(-grid_size, 3*grid_size): # who gives a fuck
 		for j in range(-grid_size, grid_size):
-			x = w//2 + CELL_WIDTH*j + (CELL_WIDTH//2)*(i%2) - BG_TILE.cx
-			y = PAD_VERTICAL + (CELL_HEIGHT//2)*(i+1) - BG_TILE.cy
+			x = w//2 + CELL_WIDTH*j + (CELL_WIDTH//2)*(i%2) - BG_TILE_OUT.cx
+			y = PAD_VERTICAL + (CELL_HEIGHT//2)*(i+1) - BG_TILE_OUT.cy
 
-			im.paste(BG_TILE.img, (x, y), BG_TILE.img)
+			im.paste(BG_TILE_OUT.img, (x, y), BG_TILE_OUT.img)
+
+	for diaglen, i, j, _ in iter_diagonals(grid_size):
+		x = w//2 + CELL_WIDTH*(j - diaglen//2) + (CELL_WIDTH//2)*(i%2) - BG_TILE_IN.cx
+		y = PAD_VERTICAL + (CELL_HEIGHT//2)*(i+1) - BG_TILE_IN.cy
+
+		im.paste(BG_TILE_IN.img, (x, y), BG_TILE_IN.img)
 
 	return im, w, h
 
 def gen_frame(n):
-	sys.stderr.write('Generating frames (#{:d})...     \r'.format(n))
+	sys.stderr.write('Generating frame #{:d}...     \r'.format(n))
 	sys.stderr.flush()
 
 	with open('data/grid_{:04d}.pkl'.format(n), 'rb') as f:
@@ -60,9 +66,9 @@ def gen_frame(n):
 		if tiles:
 			tile = randchoice(tiles)
 			x = frame_width//2 + CELL_WIDTH*(j - diaglen//2) + (CELL_WIDTH//2)*(i%2) - tile.cx
-			y = PAD_VERTICAL//2 + (CELL_HEIGHT//2)*(i+1) - tile.cy
+			y = PAD_VERTICAL + (CELL_HEIGHT//2)*(i+1) - tile.cy
 
-			im.paste(tile.img, (x, y), tile.img)
+			im.paste(tile.img.copy(), (x, y), tile.img.copy())
 
 	im.save('frames/frame_{:04d}.png'.format(n))
 
@@ -71,33 +77,42 @@ Tile = namedtuple('Tile', ['img', 'cx', 'cy'])
 
 EMPTY, TREE, LUMB = range(3)
 
-CELL_WIDTH     = 96
-CELL_HEIGHT    = 48
-PAD_HORIZONTAL = 32
-PAD_VERTICAL   = 16
+# Tweak below...
 
-BG_TILE = Tile(
-	Image.open('tiles/snow.png'.format()).convert('RGBA'),
+CELL_WIDTH     = 80
+CELL_HEIGHT    = 40
+PAD_HORIZONTAL = 20
+PAD_VERTICAL   = 80
+
+BG_TILE_OUT = Tile(
+	Image.open('tiles/snow.png').resize((CELL_WIDTH, CELL_HEIGHT)),
+	CELL_WIDTH//2,
+	CELL_HEIGHT//2
+)
+
+BG_TILE_IN = Tile(
+	Image.open('tiles/snow_field.png').resize((CELL_WIDTH, CELL_HEIGHT)),
 	CELL_WIDTH//2,
 	CELL_HEIGHT//2
 )
 
 TILES = {
 	TREE: [
-		Tile(Image.open('tiles/tree_snow1.png').convert('RGBA'), 24, 54),
-		Tile(Image.open('tiles/tree_snow2.png').convert('RGBA'), 24, 54),
-		Tile(Image.open('tiles/tree_snow3.png').convert('RGBA'), 24, 54),
-		Tile(Image.open('tiles/tree_snow4.png').convert('RGBA'), 24, 54)
+		Tile(Image.open('tiles/pine{}.png'.format(i)), 30, 77)
+		for i in (1, 2, 3, 4, 5)
 	],
 	LUMB: [
-		Tile(Image.open('tiles/lumb_snow1.png').convert('RGBA'), 26, 25),
-		Tile(Image.open('tiles/lumb_snow2.png').convert('RGBA'), 26, 25),
-		Tile(Image.open('tiles/lumb_snow3.png').convert('RGBA'), 26, 25),
-		Tile(Image.open('tiles/lumb_snow4.png').convert('RGBA'), 26, 25)
+		Tile(Image.open('tiles/lumb{}.png'.format(i)), 26, 28)
+		for i in (1, 2, 3)
+	] + [
+		Tile(Image.open('tiles/lumb{}.png'.format(i)), 26, 34)
+		for i in (4, 5)
+	] + [
+		Tile(Image.open('tiles/lumb6.png'), 26, 44)
 	]
 }
 
-NTHREADS    = 8
+# NTHREADS    = 8
 VIDEO_FNAME = 'animation.mp4'
 
 
@@ -108,17 +123,30 @@ if not os.path.isdir('data'):
 if not os.path.isdir('frames'):
 	os.mkdir('frames')
 
+sys.stderr.write('Generating background...')
+sys.stderr.flush()
+
 grid_size = get_grid_size()
 base_frame, frame_width, frame_height = gen_base_frame()
+
+sys.stderr.write('done.\n')
 
 grid_fnames = os.listdir('data')
 nframes     = len(grid_fnames)
 ffmpeg_args = ['ffmpeg', '-i', 'frames/frame_%04d.png', '-vframes', str(nframes), VIDEO_FNAME]
 
-sys.stderr.write('Starting {} threads to generate {} frames.\n'.format(NTHREADS, nframes))
+# sys.stderr.write('Starting {} threads to generate {} frames.\n'.format(NTHREADS, nframes))
 
-threadpool = Pool(NTHREADS)
-threadpool.map(gen_frame, range(nframes))
+# threadpool = Pool(NTHREADS)
+# threadpool.map(gen_frame, range(nframes))
+
+# For some reason generating frames using multiple threads makes PIL raise
+# strange errors like "OSError: image file is truncated" :/ ugh.
+
+sys.stderr.write('Generating {} frames.\n'.format(nframes))
+
+for i in range(nframes):
+	gen_frame(i)
 
 sys.stderr.write('Done! All {} frames generated.          \n'.format(nframes))
 
