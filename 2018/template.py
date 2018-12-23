e9fe8ec5dc275adbbcfe45d45165e187d94f2a89
Marco Bonelli <marco@mebeim.net>
2018-12-23 21:00:59 +0100

    Template: add default timer, better file dump.

--- 2018/template.py
+++ 2018/template.py
@@ -5,7 +5,8 @@ from utils import *
 
 advent.setup(2018, -1, dry_run=True)
 fin = advent.get_input()
-print(*fin)
+eprint(*fin, sep='')
+timer_start()
 
 ##################################################
 
