b1aaed6d626b75658f31f0e7c40478489591b61e
Marco Bonelli <marco@mebeim.net>
2018-12-24 12:07:19 +0100

    Utils: make `log_calls` a proper decorator.

--- 2018/utils/decorators.py
+++ 2018/utils/decorators.py
@@ -2,18 +2,21 @@ from functools import wraps
 
 from .helpers import log
 
-def log_calls(fn, log_return=True):
-	@wraps(fn)
-	def wrapper(*args):
-		log('{}{}\n', fn.__name__, args)
-		retval = fn(*args)
+def log_calls(log_return=True):
+	def decorating_func(fn):
+		@wraps(fn)
+		def wrapper(*args):
+			log('{}{}\n', fn.__name__, args)
+			retval = fn(*args)
 
-		if log_return:
-			log('-> {}\n', retval)
+			if log_return:
+				log('-> {}\n', retval)
 
-		return retval
+			return retval
 
-	return wrapper
+		return wrapper
+
+	return decorating_func
 
 __all__ = [
 	'log_calls'
