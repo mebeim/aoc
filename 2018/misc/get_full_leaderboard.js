f67f77b6292b22d7b89fddbbf684002f27b9cf39
Marco Bonelli <marco@mebeim.net>
2018-12-25 09:42:49 +0100

    Add full leaderboard scraper script.

---
+++ 2018/misc/get_full_leaderboard.js
@@ -0,0 +1,123 @@
+function getFullLeaderboard(excludedDays=[]) {
+	return new Promise(resolve => {
+		let users = {}
+
+		function getDay(d) {
+			if (!excludedDays.includes(d)) {
+				console.log(`Fetching day ${d}.`)
+
+				fetch(`https://adventofcode.com/2018/leaderboard/day/${d}`).then(resp => {
+					resp.text().then(text => {
+						let html    = (new DOMParser).parseFromString(text, 'text/html'),
+						    entries = html.getElementsByClassName('leaderboard-entry'),
+						    todayUsers = {}
+
+
+						for (let e of entries) {
+							let score    = 101 - parseInt(e.querySelector('.leaderboard-position').textContent),
+							    aocpp    = e.querySelector('.supporter-badge'),
+							    sponsor  = e.querySelector('.sponsor-badge'),
+							    anon     = e.querySelector('.leaderboard-anon'),
+							    link     = null,
+    						    username = null
+
+							if (aocpp)
+								e.removeChild(aocpp)
+
+							if (sponsor)
+								e.removeChild(sponsor)
+
+							link = e.querySelector('a')
+
+							if (anon) {
+								username = anon.textContent.trim().replace(')', '').replace('(', '')
+							} else {
+								if (link) {
+									username = link.textContent
+								} else {
+									while (e.children.length)
+										e.removeChild(e.children[0])
+
+									username = e.textContent.trim()
+								}
+							}
+
+							if (todayUsers[username]) {
+								todayUsers[username].score += score
+								todayUsers[username].times += 1
+							} else {
+								todayUsers[username] = {username, score, times: 1}
+
+								if (link)
+									todayUsers[username].link = link.href
+							}
+						}
+
+						for (let k of Object.keys(todayUsers)) {
+							let user = todayUsers[k]
+
+							if (users[k]) {
+								users[k].score += user.score
+								users[k].times += user.times
+								users[k].days += 1
+							} else {
+								users[k] = {
+									username: user.username,
+									score: user.score,
+									times: user.times,
+									days: 1
+								}
+
+								if (user.link)
+									users[k].link = user.link
+							}
+						}
+					})
+				})
+			} else {
+				console.log(`Skipping excluded day ${d}.`)
+			}
+
+			if (d < 25) {
+				getDay(d + 1)
+			} else if (d == 25) {
+				console.log('Done.')
+				resolve(users)
+			}
+		}
+
+		getDay(1)
+	})
+}
+
+getFullLeaderboard([6]).then(l => window.l = l)
+
+// Run after getFullLeaderBoard has finished.
+(leaderboard =>  {
+	let sorted = Object.keys(leaderboard).map(name => leaderboard[name])
+
+	sorted.sort((b, a) => a.score - b.score)
+
+	let md = '| Rank | User | Score | Times in leaderboard |\n'
+		md += '|-|-|-|-|\n'
+
+	let rank = 0, lastScore = -1, acc = 1
+
+	for (let user of sorted) {
+		if (user.score != lastScore) {
+			rank += acc
+			acc = 1
+		} else {
+			acc++
+		}
+
+		if (user.link)
+			md += `| ${rank} | [${user.username}](${user.link}) | ${user.score} | ${user.times} (${user.days} day${user.days > 1 ? 's' : ''}) |\n`
+		else
+			md += `| ${rank} | ${user.username} | ${user.score} | ${user.times} (${user.days} day${user.days > 1 ? 's' : ''}) |\n`
+
+		lastScore = user.score
+	}
+
+	copy(md)
+})(l)
