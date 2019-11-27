1a446f7d1bc17c458d83b9baadee676060e0244b
Marco Bonelli <marco@mebeim.net>
2019-11-27 04:24:44 +0100

    2019: bootstrap!

---
+++ 2019/template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import advent
+from utils import *
+
+fin = advent.get_input()
+eprint(*fin, sep='')
+timer_start()
+
+##################################################
+
+
+
+
+# advent.submit_answer(1, ans)
+
+
+
+
+# advent.submit_answer(2, ans2)
