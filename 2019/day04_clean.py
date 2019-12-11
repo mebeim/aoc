711dce021c8c5f23b840c8feeb234ae8f678baf8
Marco Bonelli <marco@mebeim.net>
2019-12-11 19:05:26 +0100

    2019 d4: better variable names.

--- 2019/day04_clean.py
+++ 2019/day04_clean.py
@@ -7,23 +7,24 @@ advent.setup(2019, 4, dry_run=True)
 fin = advent.get_input()
 
 lo, hi = map(int, fin.read().split('-'))
-n1 = n2 = 0
+n_valid1 = 0
+n_valid2 = 0
 
 for pwd in range(lo, hi + 1):
 	digits = str(pwd)
 	pairs = tuple(zip(digits, digits[1:]))
 
 	if all(a <= b for a, b in pairs) and any(a == b for a, b in pairs):
-		n1 += 1
+		n_valid1 += 1
 
 		digits = 'x' + digits + 'x'
-		isolated_pairs = zip(digits, digits[1:], digits[2:], digits[3:])
+		quadruplets = zip(digits, digits[1:], digits[2:], digits[3:])
 
-		if any(a != b and b == c and c != d for a, b, c, d in isolated_pairs):
-			n2 += 1
+		if any(a != b and b == c and c != d for a, b, c, d in quadruplets):
+			n_valid2 += 1
 
-assert n1 == 1767
-advent.submit_answer(1, n1)
+assert n_valid1 == 1767
+advent.submit_answer(1, n_valid1)
 
-assert n2 == 1192
-advent.submit_answer(2, n2)
+assert n_valid2 == 1192
+advent.submit_answer(2, n_valid2)
