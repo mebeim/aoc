6c352956c7348f08b17b4df3dc72cb6174ec8740
Marco Bonelli <marco@mebeim.net>
2019-12-15 16:02:19 +0100

    2019 d15: refactor original script to be usable.

--- 2019/day15.py
+++ 2019/day15.py
@@ -2,6 +2,7 @@
 
 import advent
 from utils import *
+import sys
 
 advent.setup(2019, 15)
 fin = advent.get_input()
@@ -9,8 +10,6 @@ fin = advent.get_input()
 timer_start()
 
 ##################################################
-
-# I'm not even bothering to try and clean this mess LOL
 
 NORTH, SOUTH, WEST, EAST = 1, 2, 3, 4
 HITWALL, MOVED, FOUNDOXYGEN = 0, 1, 2
@@ -47,19 +46,13 @@ def walk(path, stop_if_oxygen=False):
 		if res == HITWALL:
 			return res, dst
 		elif stop_if_oxygen and res == FOUNDOXYGEN:
-			print('OXYGEN', dst)
-			print('OXYGEN', dst)
-			print('OXYGEN', dst)
-			print('OXYGEN', dst)
-			print('OXYGEN', dst)
-			print('OXYGEN', dst)
 			return res, dst
 
 	assert dst is not None
 
 	return MOVED, dst
 
-def build_grid(walls):
+def build_grid(walls, oxygen_position, my_position):
 	minx = min(x for x, _ in walls)
 	maxx = max(x for x, _ in walls)
 	miny = min(y for _, y in walls)
@@ -72,75 +65,34 @@ def build_grid(walls):
 	for y in range(height):
 		for x in range(width):
 			cell = (minx + x, miny + y)
-			if cell == OXYGEN:
+			if cell == oxygen_position:
 				grid[y][x] = 'x'
-			elif cell == (0,0):
+			elif cell == my_position:
 				grid[y][x] = '@'
 			else:
 				grid[y][x] = '█' if cell in walls else ' '
 
 	return grid
 
-import sys
-import pickle
-from copy import deepcopy
+# import pickle
+# from copy import deepcopy
+# eprint('restoring walls')
+# bkp_walls = set()
+# try:
+# 	with open('walls.pkl', 'rb') as f:
+# 		bkp_walls = pickle.load(f)
+# except:
+# 	eprint('failed restore starting new')
+# walls = deepcopy(bkp_walls) if bkp_walls else set()
 
-eprint('restoring walls')
+walls = set()
 
-bkp_walls = None
-
-try:
-	with open('walls.pkl', 'rb') as f:
-		bkp_walls = pickle.load(f)
-except:
-	eprint('failed restore starting new')
-
-
-# PART 2
-OXYGEN = (-20, -14)
-walls = deepcopy(bkp_walls) if bkp_walls is not None else set()
-grid = build_grid(walls)
-dump_char_matrix(grid)
-
-G = nx.Graph()
-startpos = None
-
-for y in range(len(grid)):
-	for x, cell in enumerate(grid[y]):
-		if cell == '█':
-			continue
-
-		xy = (x, y)
-
-		if cell == 'x':
-			startpos = xy
-
-		neigh = (
-			(x+1, y),
-			(x-1, y),
-			(x, y+1),
-			(x, y-1)
-		)
-
-		for n in neigh:
-			x2, y2 = n
-			if 0 <= x2 < len(grid[0]) and 0 <= y2 < len(grid):
-				if grid[y2][x2] != '█':
-					G.add_edge(xy, n)
-
-ans = max(nx.shortest_path_length(G, startpos, node) for node in G)
-advent.submit_answer(2, ans)
-
-# PART 2
-sys.exit(0)
-
-
-minx, maxx = -100, 100
-miny, maxy = -100, 100
+minx, maxx = -50, 50
+miny, maxy = -50, 50
 tosee = set()
 
 G = nx.Graph()
-eprint('building graph')
+# eprint('building graph')
 
 for x in range(minx, maxx+1):
 	for y in range(miny, maxy+1):
@@ -163,28 +115,22 @@ for x in range(minx, maxx+1):
 			if n not in walls:
 				G.add_edge(xy, n)
 
-	log('  {}   \r', x)
+	# log('  {}   \r', x)
 
-eprint('graph done')
+# eprint('graph done')
 
 src = (0, 0)
-
-# PART 1
-# FOUND OXYGEN
-# OXYGEN = (-20, -14)
-# tosee = set([OXYGEN])
-
 tot = len(tosee)
+oxygen_found = False
 
 try:
 	for i, dst in enumerate(tosee, 1):
-		log('{:04d}/{} check {}       \r', i, tot, dst)
-		done = False
+		log('{:04d}/{} check {}                                 \r', i, tot, dst)
 
 		if src == dst:
 			continue
 
-		while not done:
+		while True:
 			try:
 				path = nx.shortest_path(G, src, dst)
 			except nx.exception.NetworkXNoPath:
@@ -194,20 +140,20 @@ try:
 
 			log('{:04d}/{} going from {} to {}... ', i, tot, src, dst)
 
-			# part 1
-			#res, lastpos = walk(path, stop_if_oxygen=True)
-
-			# part 2
-			res, lastpos = walk(path, stop_if_oxygen=False)
-
-			eprint('WMO'[res], lastpos)
+			res, lastpos = walk(path, stop_if_oxygen=(not oxygen_found))
+			log('{} {}      \r', 'WMO'[res], lastpos)
 
 			if res == HITWALL:
 				G.remove_node(lastpos)
 				walls.add(lastpos)
 			elif res == FOUNDOXYGEN:
-				done = True
-				ans = len(path) - 1
+				eprint()
+				eprint('found oxygen')
+				oxygen_found = True
+				oxygen_position = lastpos
+				oxygen_path = nx.shortest_path(G, src, oxygen_position)
+				oxygen_distance = len(oxygen_path) - 1
+				advent.submit_answer(1, oxygen_distance)
 				break
 			elif res == MOVED:
 				break
@@ -218,10 +164,44 @@ except KeyboardInterrupt:
 
 eprint()
 
-if len(walls) > len(bkp_walls):
-	log('saving {} walls\n', len(walls))
+# if len(walls) > len(bkp_walls):
+# 	log('saving {} walls\n', len(walls))
 
-	with open('walls.pkl', 'wb') as f:
-		pickle.dump(walls, f)
+# 	with open('walls.pkl', 'wb') as f:
+# 		pickle.dump(walls, f)
 
 # advent.submit_answer(1, ans)
+
+# PART 2
+my_position = (0, 0)
+grid = build_grid(walls, oxygen_position, my_position)
+dump_char_matrix(grid)
+
+G = nx.Graph()
+startpos = None
+
+for y in range(len(grid)):
+	for x, cell in enumerate(grid[y]):
+		if cell == '█':
+			continue
+
+		xy = (x, y)
+
+		if cell == 'x': # oxygen
+			startpos = xy
+
+		neigh = (
+			(x+1, y),
+			(x-1, y),
+			(x, y+1),
+			(x, y-1)
+		)
+
+		for n in neigh:
+			x2, y2 = n
+			if 0 <= x2 < len(grid[0]) and 0 <= y2 < len(grid):
+				if grid[y2][x2] != '█':
+					G.add_edge(xy, n)
+
+ans = max(nx.shortest_path_length(G, startpos, node) for node in G)
+advent.submit_answer(2, ans)
