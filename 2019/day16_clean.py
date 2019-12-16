0f45824b4086b5d1a50ad42ba4eaecb7c95cdb76
Marco Bonelli <marco@mebeim.net>
2019-12-16 18:31:51 +0100

    2019 d16: improve clean solution.

--- 2019/day16_clean.py
+++ 2019/day16_clean.py
@@ -1,50 +1,58 @@
 #!/usr/bin/env python3
 
 import advent
+
 advent.setup(2019, 16, dry_run=True)
 fin = advent.get_input()
 
 original = list(map(int, fin.read().strip()))
-nums = original[:]
-length = len(nums)
+digits = original[:]
+length = len(digits)
 
 for _ in range(100):
-	old = nums[:]
+	old = digits[:]
 
 	for i in range(length//2 + 1):
 		j = i
-		m = i + 1
+		step = i + 1
 		cur = 0
 
 		while j < length:
-			cur += sum(old[j:j + m])
-			j += 2 * m
+			cur += sum(old[j:j + step])
+			j += 2 * step
 
-			cur -= sum(old[j:j + m])
-			j += 2 * m
+			cur -= sum(old[j:j + step])
+			j += 2 * step
 
-		nums[i] = abs(cur) % 10
+		digits[i] = abs(cur) % 10
 
-	for i in range(length - 2, length//2, -1):
-		nums[i] += nums[i + 1]
-		nums[i] %= 10
+	cusum = sum(digits[length//2 + 1:length])
 
-ans = ''.join(map(str, nums[:8]))
+	for i in range(length//2 + 1, length):
+		n = digits[i]
+		digits[i] = cusum % 10
+		cusum -= n
 
-assert ans == '84487724'
-advent.submit_answer(1, ans)
+answer = ''.join(map(str, digits[:8]))
+
+assert answer == '84487724'
+advent.submit_answer(1, answer)
 
 
 skip = int(''.join(map(str, original[:7])))
-nums = (original * 10000)[skip:]
-length = len(nums)
+assert skip >= len(original)/2
 
-for step in range(100):
-	for i in range(length - 2, -1, -1):
-		nums[i] += nums[i + 1]
-		nums[i] %= 10
+digits = (original * 10000)[skip:]
+length = len(digits)
 
-ans2 = ''.join(map(str, nums[:8]))
+for _ in range(100):
+	cusum = sum(digits)
 
-assert ans2 == '84692524'
-advent.submit_answer(2, ans2)
+	for i, n in enumerate(digits):
+		digits[i] = cusum % 10
+		cusum -= n
+
+answer = ''.join(map(str, digits[:8]))
+
+assert answer == '84692524'
+advent.submit_answer(2, answer)
