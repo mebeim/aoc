9fab9fb3d075202fa7d2f01614740a31e708be3f
Marco Bonelli <marco@mebeim.net>
2019-12-17 00:32:27 +0100

    2019 d10: better var names, check assumptions.

--- 2019/day10_clean.py
+++ 2019/day10_clean.py
@@ -4,7 +4,7 @@ import advent
 from fractions import gcd
 from math import atan2, pi as PI
 
-def direction(ax, ay, bx, by):
+def ray(ax, ay, bx, by):
 	dx, dy = bx - ax, by - ay
 	div = abs(gcd(dx, dy))
 	return dx//div, dy//div
@@ -33,15 +33,15 @@ station = None
 max_in_sight = 0
 
 for src in asteroids:
-	directions = set()
+	lines_of_sight = set()
 
 	for a in asteroids:
 		if a == src:
 			continue
 
-		directions.add(direction(*src, *a))
+		lines_of_sight.add(ray(*src, *a))
 
-	in_sight = len(directions)
+	in_sight = len(lines_of_sight)
 	if in_sight > max_in_sight:
 		max_in_sight = in_sight
 		station = src
@@ -49,21 +49,23 @@ for src in asteroids:
 assert max_in_sight == 292
 advent.submit_answer(1, max_in_sight)
 
-# This part 2 solution assumes that max_in_sight is always >= 200.
-# I.E. the 200th asteroid is destroyed in the first rotation.
 
 closest = {}
 target = 200
+
+# This part 2 solution assumes that max_in_sight is always >= target (200).
+# I.E. the 200th asteroid is destroyed in the first rotation.
+assert max_in_sight >= target
 
 for a in asteroids:
 	if a == station:
 		continue
 
-	d = direction(*station, *a)
+	r = ray(*station, *a)
 	m = manhattan(*station, *a)
 
-	if d not in closest or m < closest[d][1]:
-		closest[d] = (a, m)
+	if r not in closest or m < closest[r][1]:
+		closest[r] = (a, m)
 
 ordered = sorted(closest.values(), key=lambda am: angle(*station, *am[0]))
 chosen_x, chosen_y = ordered[target - 1][0]
