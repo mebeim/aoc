354edc4104116bfd4c6d8034b87a8ac7ca46234f
Marco Bonelli <marco@mebeim.net>
2020-01-06 18:12:38 +0100

    2019 d19: correct and optimize solution.

--- 2019/day19_clean.py
+++ 2019/day19_clean.py
@@ -7,37 +7,39 @@ from lib.intcode import intcode_oneshot
 def run(inp):
 	return next(intcode_oneshot(program, inp))
 
+def get_width(x, target):
+	for top in count(0):
+		if run((x, top)) == 1:
+			break
+
+	if run((x, top + target)) == 0:
+		return 0, 0
+
+	for bottom in count(top + target + 1):
+		if run((x, bottom)) == 0:
+			break
+
+	y = bottom - target
+
+	for width in count(1):
+		if run((x + width, y)) == 0:
+			break
+
+	return width, y
+
 def bin_search(lo, hi, target):
 	best = None
 
 	while hi - lo > 1:
 		x = (lo + hi) // 2
 
-		for top in count(0):
-			if run((x, top)) == 1:
-				break
+		width, y = get_width(x, target)
 
-		if run((x, top + target)) == 0:
-			lo = x
-			continue
-
-		for bottom in count(top + target + 1):
-			if run((x, bottom)) == 0:
-				break
-
-		y = bottom - target
-
-		for fit in count(1):
-			if run((x + fit, y)) == 0:
-				break
-
-		if fit > target:
-			hi = x
-		elif fit < target:
-			lo = x
-		elif fit == target:
+		if width > target:
 			hi = x
 			best = (x, y)
+		elif width < target:
+			lo = x
 
 	return best
 
@@ -51,9 +53,17 @@ total = sum(map(run, product(range(50), range(50))))
 assert total == 152
 advent.submit_answer(1, total)
 
-closest = bin_search(10, 5000, 100)
-assert closest is not None
-answer = closest[0] * 10000 + closest[1]
+
+TARGET = 100
+bestx, besty = bin_search(10, 9999, TARGET)
+
+for x in range(bestx, bestx - 10, -1):
+	width, y = get_width(x, TARGET)
+
+	if width >= TARGET:
+		bestx, besty = x, y
+
+answer = bestx * 10000 + besty
 
 assert answer == 10730411
 advent.submit_answer(2, answer)
