8094091f453a053921a4eb00bcef07d3af50c0d2
Marco Bonelli <marco@mebeim.net>
2020-12-05 07:58:30 +0100

    2020 d5: simplify solution

--- 2020/solutions/day05.py
+++ 2020/solutions/day05.py
@@ -6,25 +6,15 @@ from itertools import product
 advent.setup(2020, 5)
 fin = advent.get_input()
 
-table = str.maketrans('BFRL', '1010')
-seats = fin.read().translate(table).splitlines()
-seats = list(map(lambda s: (int(s[:7], 2), int(s[7:], 2)), seats))
-max_id = 0
-
-for row, col in seats:
-	seat_id = row * 8 + col
-
-	if seat_id > max_id:
-		max_id = seat_id
+table  = str.maketrans('BFRL', '1010')
+seats  = fin.read().translate(table).splitlines()
+ids    = tuple(map(lambda s: int(s, 2), seats))
+max_id = max(ids)
 
 advent.print_answer(1, max_id)
 
-
-seats = set(seats)
-
-for r, c in product(range(1, 127), range(1, 7)):
-	if (r, c) not in seats and (r, c - 1) in seats and (r, c + 1) in seats:
-		my_id = r * 8 + c
-		break
+min_id   = min(ids)
+expected = max_id * (max_id + 1) // 2 - min_id * (min_id - 1) // 2
+my_id    = expected - sum(ids)
 
 advent.print_answer(2, my_id)
