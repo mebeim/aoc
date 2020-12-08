b2b6b05e7851c35fa6eb0d38d0f7b2ccbd76c52f
Marco Bonelli <marco@mebeim.net>
2020-12-08 21:18:04 +0100

    Utils/advent: simplify logging

--- utils/advent.py
+++ utils/advent.py
@@ -5,17 +5,25 @@ import re
 from importlib import find_loader
 from datetime import datetime, timedelta
 
-from .helpers import log
+def log(s, *a):
+	sys.stderr.write('[advent] ' + s.format(*a))
+	sys.stderr.flush()
+
+def logcont(s, *a):
+	sys.stderr.write(s.format(*a))
+	sys.stderr.flush()
 
 def check_or_die(resp):
 	if resp.status_code != 200:
-		log('\n[advent] ERROR: response {}, url: {}\n', resp.status_code, resp.url)
-		log('[advent] Did you log in and update your session cookie?\n')
+		logcont('\n')
+		log('ERROR: response {}, url: {}\n', resp.status_code, resp.url)
+		log('Did you log in and update your session cookie?\n')
 		sys.exit(1)
 
 	if 'please identify yourself' in resp.text.lower():
-		log('\n[advent] ERROR: Server returned 200, but is asking for identification.\n')
-		log('[advent] Did you log in and update your session cookie?\n')
+		logcont('\n')
+		log('ERROR: Server returned 200, but is asking for identification.\n')
+		log('Did you log in and update your session cookie?\n')
 		sys.exit(1)
 
 def check_setup_once():
@@ -24,10 +32,10 @@ def check_setup_once():
 		y, m, d = now.year, now.month, now.day
 
 		if m != 12 or (m == 12 and d > 25):
-			log('[advent] ERROR: year and day not set, and no event currently running!\n')
+			log('ERROR: year and day not set, and no event currently running!\n')
 			sys.exit(1)
 
-		log('[advent] Year and day not set, assuming today: Dec {}, {}.\n', d, y)
+		log('Year and day not set, assuming today: Dec {}, {}.\n', d, y)
 		setup(y, d)
 
 def setup(year, day):
@@ -36,7 +44,7 @@ def setup(year, day):
 	global SESSION
 
 	if not (year >= 2015 and 1 <= day <= 25):
-		log('[advent] ERROR: invalid year and/or day set!\n')
+		log('ERROR: invalid year and/or day set!\n')
 		sys.exit(1)
 
 	YEAR = year
@@ -53,28 +61,28 @@ def get_input(fname=None, mode='r'):
 	if not os.path.isdir(CACHE_DIR):
 		try:
 			os.mkdir(CACHE_DIR)
-			log("[advent] Created cache directory '{}' since it did not exist.\n", CACHE_DIR)
+			log("Created cache directory '{}' since it did not exist.\n", CACHE_DIR)
 		except Exception as e:
-			log("[advent] ERROR: could not create cache directory '{}'.\n", CACHE_DIR)
-			log('[advent] {}\n', str(e))
+			log("ERROR: could not create cache directory '{}'.\n", CACHE_DIR)
+			log('{}\n', str(e))
 			sys.exit(1)
 
-	log('[advent] Getting input for year {} day {}... ', YEAR, DAY)
+	log('Getting input for year {} day {}... ', YEAR, DAY)
 
 	if fname is None:
 		fname = os.path.join(CACHE_DIR, '{}_{:02d}.txt'.format(YEAR, DAY))
 
 	if not os.path.isfile(fname):
 		if not REQUESTS:
-			log('err!\n')
-			log('[advent] ERROR: cannot download input, no requests module installed!\n')
+			logcont('err!\n')
+			log('ERROR: cannot download input, no requests module installed!\n')
 			sys.exit(1)
 		elif not SESSION:
-			log('err!\n')
-			log('[advent] ERROR: cannot download input file without session cookie!\n')
+			logcont('err!\n')
+			log('ERROR: cannot download input file without session cookie!\n')
 			sys.exit(1)
 
-		log('downloading... ')
+		logcont('downloading... ')
 
 		r = S.get(URL.format(YEAR, DAY, 'input'))
 		check_or_die(r)
@@ -82,10 +90,10 @@ def get_input(fname=None, mode='r'):
 		with open(fname, 'wb') as f:
 			f.write(r.content)
 
-		log('done.\n')
+		logcont('done.\n')
 
 	else:
-		log('done (from disk).\n')
+		logcont('done (from disk).\n')
 
 	return open(fname, mode)
 
@@ -96,35 +104,35 @@ def submit_answer(part, answer):
 	check_setup_once()
 
 	if not REQUESTS:
-		log('[advent] Cannot upload answer, no requests module installed!\n')
+		log('Cannot upload answer, no requests module installed!\n')
 		print_answer(part, answer)
-		return True
+		return False
 
-	log('[advent] Submitting day {} part {} answer: {}\n', DAY, part, answer)
+	log('Submitting day {} part {} answer: {}\n', DAY, part, answer)
 
 	r = S.post(URL.format(YEAR, DAY, 'answer'), data={'level': part, 'answer': answer})
 	check_or_die(r)
 	t = r.text.lower()
 
 	if 'did you already complete it' in t:
-		log('[advent] Already completed or wrong day/part.\n')
+		log('Already completed or wrong day/part.\n')
 		return False
 
 	if "that's the right answer" in t:
-		log('[advent] Right answer!\n')
+		log('Right answer!\n')
 		return True
 
 	if 'you have to wait' in t:
 		matches = re.compile(r'you have ([\w ]+) left to wait').findall(t)
 
 		if matches:
-			log('[advent] Submitting too fast, {} left to wait.\n', matches[0])
+			log('Submitting too fast, {} left to wait.\n', matches[0])
 		else:
-			log('[advent] Submitting too fast, slow down!\n')
+			log('Submitting too fast, slow down!\n')
 
 		return False
 
-	log('[advent] Wrong answer :(\n')
+	log('Wrong answer :(\n')
 	return False
 
 URL       = 'https://adventofcode.com/{:d}/day/{:d}/{:s}'
