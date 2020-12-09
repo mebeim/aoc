642343815b389cf19a44a3216b0d26dbab98fae0
Marco Bonelli <marco@mebeim.net>
2020-12-09 06:16:48 +0100

    2020 d9: add input and original solution

---
+++ 2020/original_solutions/day09.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from utils.all import *
+from lib.vm import VM, VMRuntimeError
+
+advent.setup(2020, 9)
+fin = advent.get_input()
+timer_start()
+
+ints = get_ints(fin, True)
+
+for k in range(25, len(ints)):
+	ok = False
+	c = ints[k]
+
+	for i in range(k - 25, k):
+		for j in range(k - 25, k):
+			if i == j:
+				continue
+			a, b = ints[i], ints[j]
+
+			if a + b == c:
+				ok = True
+				break
+
+		if ok:
+			break
+
+	if not ok:
+		ans = c
+		break
+
+advent.submit_answer(1, ans)
+
+c = 32321523
+for i in range(len(ints)):
+	for j in range(i + 1, len(ints)):
+		if sum(ints[i:j]) == c:
+			M = max(ints[i:j])
+			m = min(ints[i:j])
+			ans2 = m + M
+
+			advent.submit_answer(2, ans2)
+			sys.exit(0)
