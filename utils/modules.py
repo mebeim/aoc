ed2ba12171f242778f5cd71ba9fbeaaa7f9814be
Marco Bonelli <marco@mebeim.net>
2021-12-11 23:38:34 +0100

    Utils/modules: import count from itertools

--- utils/modules.py
+++ utils/modules.py
@@ -9,7 +9,7 @@ from copy import deepcopy
 from functools import lru_cache, reduce, partial
 from collections import defaultdict, deque, namedtuple, Counter
 from operator import itemgetter, attrgetter, methodcaller
-from itertools import product, permutations, combinations, filterfalse, starmap
+from itertools import product, permutations, combinations, filterfalse, starmap, count
 from math import pi as PI, inf as INFINITY
 from math import floor, ceil, sqrt, sin, cos, tan, asin, acos, atan, factorial
 
