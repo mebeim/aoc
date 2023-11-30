8f3b8dee2b48bbc0ca4cc928e9635bb39351ac9d
Marco Bonelli <marco@mebeim.net>
2023-11-30 17:11:18 +0100

    utils: Add decorator for automated recursive logging
    Might seem like a weird abomination, but it's pretty cool.

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,8 +1,35 @@
-__all__ = ['log_calls', 'selective_cache']
+__all__ = ['log_calls', 'log_calls_recursive', 'selective_cache']
 
-from functools import wraps, lru_cache
+from sys import _getframe
+from functools import wraps
+from itertools import count, starmap, chain
 from inspect import signature, Parameter
-from .helpers import log
+
+from .helpers import log, rlog, reprint
+
+# Adapted from: https://stackoverflow.com/a/47956089/3889449
+def _stack_size(size_hint=8, getframe=_getframe):
+	'''Get stack size for caller's frame.'''
+	frame = None
+	try:
+		while True:
+			frame = getframe(size_hint)
+			size_hint *= 2
+	except ValueError:
+		if frame:
+			size_hint //= 2
+		else:
+			while not frame:
+				size_hint = max(2, size_hint // 2)
+				try:
+					frame = getframe(size_hint)
+				except ValueError:
+					continue
+
+	for size in count(size_hint):
+		frame = frame.f_back
+		if not frame:
+			return size
 
 def log_calls(log_return=True):
 	'''Decorate a function logging arguments and return value
@@ -16,6 +43,106 @@ def log_calls(log_return=True):
 
 			if log_return:
 				log('-> {}\n', retval)
+
+			return retval
+		return wrapper
+	return decorator
+
+def log_calls_recursive(log_call=True, log_return=True):
+	'''Decorate a function logging arguments (if log_call=True) and return value
+	(if log_return=True) of every call to standard error, indenting the logs for
+	recursive calls according to the recursion level.
+
+	Additionally, add .log() and .eprint() methods to the decorated function
+	that will act as log() or eprint(), but add indentation according to the
+	current recursion level.
+
+	Usage example:
+
+		@log_calls_recursive()
+		def fib(n):
+			if n == 0 or n == 1:
+				return n
+
+			fib.eprint('hello!')
+
+			return n + fib(n - 1) + fib(n - 2)
+
+		>>> fib(3)
+		 ┌ fib(3)
+		 │ hello!
+		 │ ┌ fib(2)
+		 │ │ hello!
+		 │ │ ┌ fib(1)
+		 │ │ └> 1
+		 │ │ ┌ fib(0)
+		 │ │ └> 0
+		 │ └> 3
+		 │ ┌ fib(1)
+		 │ └> 1
+		 └> 7
+	'''
+	# Explanation for the depth calculation:
+	#
+	# Since we are wrapping the function, every time the function is called we
+	# actually add 2 call frames instead of 1, because wrapper() is called,
+	# which then calls the real function:
+	#
+	#     depth = _stack_size() // 2
+	#
+	# Additionally, since the logging is done eithr through logfunc() or
+	# through eprintfunc(), there is always one more frame to discard:
+	#
+	#     depth = (_stack_size() - 1) // 2
+	#
+	# If logfunc() or eprintfunc() are called by the user with func.log() or
+	# func.eprint() inside the decorated function, then we have yet another
+	# frame to discard:
+	#
+	#     depth = (_stack_size() - 1 - 1) // 2
+	#
+	# We use _user_call to distinguish between an internal call (_user_call=0)
+	# or one made by the user (_user_call=1):
+	#
+	#     depth = (_stack_size() - 1 - _user_call) // 2
+
+	def decorator(fn):
+		initial_depth = None
+
+		def logfunc(*a, _user_call=1, **kwa):
+			nonlocal initial_depth
+
+			depth = (_stack_size() - 1 - _user_call) // 2
+			if initial_depth is None:
+				initial_depth = depth
+
+			rlog(depth - initial_depth)(*a, **kwa)
+
+		def eprintfunc(*a, _user_call=1, **kwa):
+			nonlocal initial_depth
+
+			depth = (_stack_size() - 1 - _user_call) // 2
+			if initial_depth is None:
+				initial_depth = depth
+
+			reprint(depth - initial_depth)(*a, **kwa)
+
+		fn.log = logfunc
+		fn.eprint = eprintfunc
+
+		@wraps(fn)
+		def wrapper(*args, **kwargs):
+			repr_args = map(repr, args)
+			repr_kwargs = starmap('{}={!r}'.format, kwargs.items())
+			signature = ', '.join(chain(repr_args, repr_kwargs))
+
+			if log_call:
+				fn.log('{}({})\n', fn.__name__, signature, _user_call=0, is_header=True)
+
+			retval = fn(*args, **kwargs)
+
+			if log_return:
+				fn.eprint(retval, _user_call=0, is_retval=True)
 
 			return retval
 		return wrapper
