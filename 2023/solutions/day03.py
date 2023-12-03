65a7631a4cce5e2f121159c6a3061e8caf19a74b
Marco Bonelli <marco@mebeim.net>
2023-12-03 14:28:00 +0100

    2023 d3: Simplify and optimize solution

--- 2023/solutions/day03.py
+++ 2023/solutions/day03.py
@@ -2,34 +2,26 @@
 
 import sys
 from math import prod
-from collections import defaultdict, deque
+from collections import defaultdict
 
-def neighbors(grid, r, c, h, w):
-	for dr, dc in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-		rr, cc = r + dr, c + dc
-		if 0 <= rr < h and 0 <= cc < w:
-			yield rr, cc, grid[rr][cc]
+def adjacent_symbols(grid, r, start_c, height, width):
+	row   = grid[r]
+	above = grid[r - 1] if r > 0 else []
+	below = grid[r + 1] if r < height - 1 else []
 
-def adjacent_symbols(grid, r, c, h, w):
-	queue = deque(neighbors(grid, r, c, h, w))
-	visited = set()
-	res = []
+	for c in range(max(0, start_c - 1), width):
+		if above and above[c] not in '0123456789.':
+			yield (r - 1, c)
 
-	while queue:
-		r, c, char = queue.popleft()
-		if (r, c) in visited:
-			continue
+		if below and below[c] not in '0123456789.':
+			yield (r + 1, c)
 
-		visited.add((r, c))
+		if row[c] not in '0123456789':
+			if row[c] != '.':
+				yield (r, c)
 
-		if char not in '0123456789':
-			if char != '.':
-				res.append((r, c))
-			continue
-
-		queue.extend(neighbors(grid, r, c, h, w))
-
-	return res
+			if c > start_c:
+				break
 
 def extract_number(row, start, length):
 	end = start + 1
@@ -45,6 +37,7 @@ fin = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
 grid = list(map(str.rstrip, fin.readlines()))
 height, width = len(grid), len(grid[0])
 adj_numbers = defaultdict(list)
+answer1 = answer2 = 0
 
 for r, row in enumerate(grid):
 	c = 0
@@ -56,9 +49,11 @@ for r, row in enumerate(grid):
 		if c >= width:
 			break
 
-		symbols = adjacent_symbols(grid, r, c, height, width)
+		symbols = list(adjacent_symbols(grid, r, c, height, width))
+
 		if symbols:
 			number = extract_number(row, c, width)
+			answer1 += number
 
 			for sym in symbols:
 				adj_numbers[sym].append(number)
@@ -66,7 +61,6 @@ for r, row in enumerate(grid):
 		while c < width and row[c].isdigit():
 			c += 1
 
-answer1 = sum(map(sum, adj_numbers.values()))
 print('Part 1:', answer1)
 
 answer2 = sum(map(prod, filter(lambda x: len(x) == 2, adj_numbers.values())))
