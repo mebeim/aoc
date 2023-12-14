89670553882f558cfde4da484e93f023d9f8c276
Marco Bonelli <marco@mebeim.net>
2023-12-14 19:16:09 +0100

    2023 d14: Optimize solution

--- 2023/solutions/day14.py
+++ 2023/solutions/day14.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
 
 import sys
+from operator import itemgetter
 
 def rotate_90(coords, height):
 	return set((c, height - r - 1) for r, c in coords)
 
-def move(spheres, cubes):
-	new_spheres = set()
+def move(fixed, movable):
+	new_movable = fixed.copy()
 
-	for r, c in sorted(spheres):
+	for r, c in sorted(movable, key=itemgetter(0)):
 		r -= 1
-		while r >= 0 and (r, c) not in cubes and (r, c) not in new_spheres:
+		while r >= 0 and (r, c) not in new_movable:
 			r -= 1
 
-		new_spheres.add((r + 1, c))
+		new_movable.add((r + 1, c))
 
-	return new_spheres
+	return new_movable - fixed
 
-def spin(spheres, cubes, height, iterations):
-	seen = {frozenset(spheres): 0}
+def spin(fixed, movable, height, width, iterations):
+	seen = {frozenset(movable): 0}
 
-	cached_cubes = [cubes]
+	cache = [(fixed, height)]
 	for _ in range(3):
-		cached_cubes.append(rotate_90(cached_cubes[-1], height))
+		cache.append((rotate_90(cache[-1][0], height), width))
+		height, width = width, height
 
 	for i in range(1, iterations + 1):
-		for cubes in cached_cubes:
-			spheres = rotate_90(move(spheres, cubes), height)
+		for fixed, height in cache:
+			movable = rotate_90(move(fixed, movable), height)
 
-		key = frozenset(spheres)
+		key = frozenset(movable)
 		if key in seen:
 			break
 
 		seen[key] = i
 		seen[i]   = key
 
-	offset    = seen[key]
-	cycle_len = i - offset
-	remaining = iterations - offset
-	final     = remaining % cycle_len + offset
+	cycle_start = seen[key]
+	cycle_len   = i - cycle_start
+	remaining   = iterations - cycle_start
+	final       = remaining % cycle_len + cycle_start
 
 	return sum(map(lambda rc: height - rc[0], seen[final]))
 
@@ -46,21 +48,20 @@ def spin(spheres, cubes, height, iterations):
 # Open the first argument as input or use stdin if no arguments were given
 fin = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
 
-grid    = fin.read().splitlines()
-height  = len(grid)
-width = len(grid[0]) # test
-cubes   = set()
-spheres = set()
+grid = fin.read().splitlines()
+height, width = len(grid), len(grid[0])
+fixed = set()
+movable = set()
 
 for r, row in enumerate(grid):
 	for c, char in enumerate(row):
 		if char == '#':
-			cubes.add((r, c))
+			fixed.add((r, c))
 		elif char == 'O':
-			spheres.add((r, c))
+			movable.add((r, c))
 
-total1 = sum(map(lambda rc: height - rc[0], move(spheres, cubes)))
+total1 = sum(map(lambda rc: height - rc[0], move(fixed, movable)))
 print('Part 1:', total1)
 
-total2 = spin(spheres, cubes, height, 1000000000)
+total2 = spin(fixed, movable, height, width, 1000000000)
 print('Part 2:', total2)
