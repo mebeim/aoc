12ec8739a9f58a23e9acd720a9baa8d3dfc354ab
Marco Bonelli <marco@mebeim.net>
2024-12-23 07:02:55 +0100

    2024 d22: Optimize solution, calc p1 and p2 together

--- 2024/solutions/day22.py
+++ 2024/solutions/day22.py
@@ -4,17 +4,8 @@ import sys
 from collections import defaultdict
 
 
-def transform(n):
-	for _ in range(2000):
-		n = ((n << 6) ^ n) & 0xffffff
-		n = ((n >> 5) ^ n) & 0xffffff
-		n = ((n << 11) ^ n) & 0xffffff
-
-	return n
-
-
 def map_sequence(n):
-	value_map = {}
+	diff_map = {}
 	prev = n % 10
 	key = 0
 
@@ -29,30 +20,32 @@ def map_sequence(n):
 			# Use key as a compact hash of the last 4 diff values, using 5 bits
 			# per value ([-9, 9] -> [1, 19]) with 0 meaning "no value".
 			key = ((key & 0x7fff) << 5) | (value - prev + 10)
-			if key not in value_map:
-				value_map[key] = value
+			if key not in diff_map:
+				diff_map[key] = value
 
 		prev = value
 
-	return value_map
+	return n, diff_map
 
 
 def solve(numbers):
-	total_value = defaultdict(int)
+	total_final_values = 0
+	sequence_value = defaultdict(int)
 
-	for value_map in map(map_sequence, numbers):
-		for seq, value in value_map.items():
-			total_value[seq] += value
+	for final_value, diff_map in map(map_sequence, numbers):
+		total_final_values += final_value
 
-	return max(total_value.values())
+		for seq, value in diff_map.items():
+			sequence_value[seq] += value
+
+	return total_final_values, max(sequence_value.values())
 
 
 # Open the first argument as input or use stdin if no arguments were given
 fin = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
 
 numbers = list(map(int, fin.read().splitlines()))
-ans1 = sum(map(transform, numbers))
-print('Part 1:', ans1)
 
-ans2 = solve(numbers)
+ans1, ans2 = solve(numbers)
+print('Part 1:', ans1)
 print('Part 2:', ans2)
